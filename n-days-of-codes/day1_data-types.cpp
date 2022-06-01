bdfe8a64e0b1319e1425b25740f109f75a4448dd
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-01 16:48:13 -0600

    C++ Day 1

---
+++ n-days-of-codes/day1_data-types.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int a;
+    double b;
+    string c {};
+    // Read and save an integer, double, and String to your variables.
+    
+    cin >> a;
+    cin >> b;
+    cin.ignore();
+    getline(cin,c); 
+
+    
+    // Print the sum of both integer variables on a new line.
+    cout << i+a <<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<fixed<<setprecision(1)<<d+b<<endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s+c <<endl;
+
+    return 0;
+}
