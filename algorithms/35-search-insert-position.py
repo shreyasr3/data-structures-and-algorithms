56b503b2faa08ba5d6aa32fe6856b4f7682a0ffb
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-23 00:48:32 -0600

    35. Search Insert Position
    Runtime: 110 ms, faster than 5.46% of Python3 online submissions for Search Insert Position.
    Memory Usage: 14.6 MB, less than 82.21% of Python3 online submissions for Search Insert Position.

---
+++ algorithms/35-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+        
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if target == nums[mid]:
+                return mid
+            if target < nums[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
+      
+      
+      
+      
+      
+      #Python library
+      
+class Solution:
+    def searchInsert(self, nums, target):
+        return bisect.bisect_left(nums, target)
+     
