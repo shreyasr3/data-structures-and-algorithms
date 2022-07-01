0469f71bcb20aeb337a774cedb85dfe5c1aeaacf
shreyasr3 <106269811+shreyasr3@users.noreply.github.com>
2022-06-30 19:47:48 -0600

    Update 557-reverse-words-in-string-III.py

--- algorithms/557-reverse-words-in-string-III.py
+++ algorithms/557-reverse-words-in-string-III.py
@@ -3,5 +3,22 @@ class Solution:
         str_list = s.split()
         return " ".join([sl[::-1] for sl in str_list])
         
+     
+## O(n)
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        right, left = 0, 0
+        res = []
         
+        while right < len(s):
+            if s[right] == " ":
+                res.append(s[left:right][::-1])
+                left = right+1
+        
+            right += 1
+        
+        res.append(s[left:right][::-1])
+        return " ".join(res)
+       
     
